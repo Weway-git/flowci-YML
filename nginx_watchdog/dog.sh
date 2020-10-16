c7ff6b687e36c484cc66847d47af87988f72cd4a
zhowwei <1419864987@qq.com>
2020-10-16 10:39:56 +0800

    加入了发送邮件功能

--- nginx_watchdog/dog.sh
+++ nginx_watchdog/dog.sh
@@ -1,20 +1,26 @@
 function start(){
     echo haha
-    local file_path=/root/nginx_log_watchdog/haha.log
-    local check_time=5
+    local file_path=/var/log/nginx
+    local file_name=access.log
+    local check_time=2
     while :
     do
-        file_old_stat="`du -b haha.log`"
-        echo ${file_old_stat}
+        file_old_stat="`du -b ${file_path}/${file_name}`"
+        echo old${file_old_stat}
         sleep ${check_time}
-        file_new_stat="`du -b haha.log`"
-        echo ${file_new_stat}
+        file_new_stat="`du -b ${file_path}/${file_name}`"
+        echo filenew${file_new_stat}
         if [[ `echo ${file_old_stat}` == `echo ${file_new_stat}` ]]; then
-                        
+            # not change
+            echo "not change"
         else
-            echo "#### Wait ${check_time}s ####"
+            # changed
+            date=`date`
+            mail_content=`cat ${file_path}/${file_name} | awk 'END {print}'`
+            # echo mail_content  ${mail_content}
             echo "changed!!!!!!!!!!!!!!!!!!!!!!"
-            echo "haha" |mail -s ${a} 1419864987@qq.com
+            echo "${mail_content}" |mail -s "${date}" 1419864987@qq.com
+            sleep 10
         fi
     done
 }
