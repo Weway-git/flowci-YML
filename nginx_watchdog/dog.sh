3cd20221e41ff2c9714c0ddc154abee779bc66e1
Weway-git <1419864987@qq.com>
2020-10-17 00:56:59 +0800

    优化分隔符和参数

--- nginx_watchdog/dog.sh
+++ nginx_watchdog/dog.sh
@@ -1,7 +1,7 @@
 function start(){
     local file_path=/usr/local/nginx/logs
     local file_name=access.log
-    local check_time=2
+    local check_time=0.1
     while :
     do
         file_old_stat="`du -b ${file_path}/${file_name}`"
@@ -17,12 +17,12 @@ function start(){
             export LANG=zh_CN.UTF-8
             date=`date`
             content=`cat ${file_path}/${file_name} | awk 'END {print}'`
-            ip=`echo ${content} | awk -F- '{print $1}'`
-            time=`echo ${content} | awk -F- '{print $2}'`
-            method=`echo ${content} | awk -F- '{print $3}'`
-            code=`echo ${content} | awk -F- '{print $4}'`
-            size=`echo ${content} | awk -F- '{print $5}'`
-            user_agent=`echo ${content} | awk -F- '{print $6}'`
+            ip=`echo ${content} | awk -F '|'  '{print $1}'`
+            time=`echo ${content} | awk -F '|'  '{print $3}'`
+            method=`echo ${content} | awk -F '|'  '{print $4}'`
+            code=`echo ${content} | awk -F '|'  '{print $5}'`
+            # size=`echo ${content} | awk -F- '{print $5}'`
+            user_agent=`echo ${content} | awk -F '|'  '{print $7}'`
             
 
             # ip to address
@@ -30,10 +30,10 @@ function start(){
 
             # echo content  ${content}
             echo "changed!!!!!!!!!!!!!!!!!!!!!!"
-            echo -e "IPaddr is ${ip_addr}\n IP address is ${ip}\n UserAgent is ${user_agent}\n Time is ${time}\n check time is ${check_time}" >  /root/watchdogs/temp.txt
+            echo -e "[IPaddr]: ${ip_addr}\n [IP address]: ${ip}\n [UserAgent]: ${user_agent}\n [Time]: ${time}\n [check time]: ${check_time}" >  /root/watchdogs/temp.txt
             dos2unix -k /root/watchdogs/temp.txt
             mail -s "${date}" 1419864987@qq.com < /root/watchdogs/temp.txt
-            sleep 2
+            sleep 1
             echo "mail sent successfully!"
         fi
     done
