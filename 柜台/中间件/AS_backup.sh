312a248df93138e4ea7f7479323ec205f7a3acef
zhowwei <1419864987@qq.com>
2020-12-21 11:24:28 +0800

    中间件备份添加输出

--- 柜台/中间件/AS_backup.sh
+++ 柜台/中间件/AS_backup.sh
@@ -3,3 +3,5 @@ mkdir -p /home/h3c_file/AS_backup|true
 date1=$(date +%F-%H%M%S)
 mkdir -p /home/h3c_file/AS_backup/$date1
 cp  /home/hundsun/workspace/nrs.xml   /home/h3c_file/AS_backup/$date1
+echo "nrs.xml before sync:"
+cat /home/h3c_file/AS_backup/$date1/nrs.xml
