b05589f2496fdb0b3433b425227ce84f7a3a59c8
zhowwei <1419864987@qq.com>
2021-01-04 09:48:48 +0800

    复制脚本和数据库部署检查项目检查

--- 柜台/agent启动脚本/脚本复制脚本.sh
+++ 柜台/agent启动脚本/脚本复制脚本.sh
@@ -1,10 +1,10 @@
 # 目标环境：------------------------
 # 目录建立
-ssh root@192.168.135.102  " mkdir -p  /home/h3c_file/" & exit
+ssh root@192.25.101.13  " mkdir -p  /home/h3c_file/" & exit
 # 脚本拷贝
-scp *  root@192.168.135.102:/home/h3c_file/
+scp *  root@192.25.101.13:/home/h3c_file/
 # 免密
-ssh-copy-id -i .ssh/id_rsa.pub root@192.168.135.102
+ssh-copy-id -i .ssh/id_rsa.pub root@192.25.101.13
 
 # 数据库源环境:------------------------------
 # 数据库拷贝时源数据库vsftpd搭建
@@ -15,8 +15,14 @@ passwd ftpuser
 # 数据库目标环境:---------------------
 
 # ftp客户端安装
-rpm 
+rpm -ivh ftp-0.17-54.el6.x86_64.rpm
+
+# java
+rpm -ivh jre-8u271-linux-x64.rpm
 
 
 # crontab 每天一点执行
 0 1 * * ?
+
+# too open
+chmod 600 
